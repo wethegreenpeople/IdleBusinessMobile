4b98c0d6af1dd5d10cbc96d0c1e9e2ed5ac952d7
John Singh <wethegreenpeople@gmail.com>
2021-04-21 11:06:17 -0700

    Fix issue with sector page height being incorrect.

--- lib/Views/Sector/Sector.dart
+++ lib/Views/Sector/Sector.dart
@@ -5,6 +5,7 @@ import 'package:idlebusiness_mobile/Views/PurchaseAssets/PurchaseAssets.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:idlebusiness_mobile/Stores/BusinessStore.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+import 'package:sizer/sizer.dart';
 
 class SectorPage extends StatefulWidget {
   Business _business;
@@ -31,53 +32,47 @@ class _SectorPageState extends State<SectorPage> {
           child: Column(
             mainAxisSize: MainAxisSize.max,
             children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                child: Expanded(
-                  child: SizedBox(
-                    height: MediaQuery.of(context).size.height * .75,
-                    child: ListView(
-                      children: [
-                        Card(
-                            color: Colors.white,
-                            child: ListTile(
-                                leading: Icon(Icons.info),
-                                title: Text("Joining a sector"),
-                                subtitle: RichText(
-                                    text: TextSpan(
-                                  children: [
-                                    TextSpan(
-                                        style: TextStyle(color: Colors.grey),
-                                        text:
-                                            "In addition to gaining the bonuses listed below each sector, once you join a sector you will belong to a group of businesses that can choose to help each other out. \n\nAfter joining a sector you will unlock bonus items whose stats are applied to ALL businesses within that sector.\n\nIf you see the globe icon ("),
-                                    WidgetSpan(
-                                        child: Icon(
-                                      MdiIcons.earth,
-                                      size: 20,
-                                    )),
-                                    TextSpan(
-                                        style: TextStyle(color: Colors.grey),
-                                        text:
-                                            "), the bonus will be applied to all businesses within your sector"),
-                                  ],
-                                )))),
-                        _sectorCard("Tech", "-10% Item Cost For All Items",
-                            Icon(Icons.computer), _business.id, 1),
-                        _sectorCard(
-                            "Marketing",
-                            "-10% Employee Cost For All Employees",
-                            Icon(Icons.perm_media),
-                            _business.id,
-                            3),
-                        _sectorCard(
-                            "Real Estate",
-                            "No Cash Per Second fee on real estate",
-                            Icon(Icons.house),
-                            _business.id,
-                            2),
-                      ],
-                    ),
-                  ),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Card(
+                        color: Colors.white,
+                        child: ListTile(
+                            leading: Icon(Icons.info),
+                            title: Text("Joining a sector"),
+                            subtitle: RichText(
+                                text: TextSpan(
+                              children: [
+                                TextSpan(
+                                    style: TextStyle(color: Colors.grey),
+                                    text:
+                                        "In addition to gaining the bonuses listed below each sector, once you join a sector you will belong to a group of businesses that can choose to help each other out. \n\nAfter joining a sector you will unlock bonus items whose stats are applied to ALL businesses within that sector.\n\nIf you see the globe icon ("),
+                                WidgetSpan(
+                                    child: Icon(
+                                  MdiIcons.earth,
+                                  size: 20,
+                                )),
+                                TextSpan(
+                                    style: TextStyle(color: Colors.grey),
+                                    text:
+                                        "), the bonus will be applied to all businesses within your sector"),
+                              ],
+                            )))),
+                    _sectorCard("Tech", "-10% Item Cost For All Items",
+                        Icon(Icons.computer), _business.id, 1),
+                    _sectorCard(
+                        "Marketing",
+                        "-10% Employee Cost For All Employees",
+                        Icon(Icons.perm_media),
+                        _business.id,
+                        3),
+                    _sectorCard(
+                        "Real Estate",
+                        "No Cash Per Second fee on real estate",
+                        Icon(Icons.house),
+                        _business.id,
+                        2),
+                  ],
                 ),
               )
             ],
